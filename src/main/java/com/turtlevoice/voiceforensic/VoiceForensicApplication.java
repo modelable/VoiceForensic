9e6735ffb4d9873e6e61a17790d5712752b25d5a
이기정 <kee@kee.local>
2024-06-24 15:24:34 +0900

    first commit

---
+++ src/main/java/com/turtlevoice/voiceforensic/VoiceForensicApplication.java
@@ -0,0 +1,13 @@
+package com.turtlevoice.voiceforensic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceForensicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceForensicApplication.class, args);
+    }
+
+}
